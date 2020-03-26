37fff3c6d38f1142156638b796af33a61dae8df7
Dosya <alybaev_d@auca.kg>
2020-03-27 01:29:28 +0600

    Add Teams Statistics in Leagues

--- TeamsPart.sql
+++ TeamsPart.sql
@@ -1,18 +1,57 @@
 
 USE FootbalTeams
-
+DROP TABLE IF EXISTS TeamsLeaguesStatistics,TeamsStatisticsTypes, TimePeriods
 DROP TABLE IF EXISTS RankingTeams, Standings
-DROP TABLE IF EXISTS ParametresTeams,  TeamAttributes,Parametres,TeamsMatches,  Matches,  RankingSystems
-DROP TABLE IF EXISTS  Teams, TeamGenders, GroupsAge, TeamTypes
-
-CREATE TABLE TeamGenders (
+DROP TABLE IF EXISTS  TeamsMatches,  Matches,  RankingSystems
+DROP TABLE IF EXISTS Teams, TeamGenders, AgeCategories, TeamTypes
+CREATE TABLE TimePeriods (
 	ID INT NOT NULL 
-		CONSTRAINT PK_TeamGenders PRIMARY KEY CLUSTERED,
-	Gender BIT
+		CONSTRAINT PK_TimePeriods PRIMARY KEY CLUSTERED,
 )
-CREATE TABLE GroupsAge (
+CREATE TABLE TeamsStatisticsTypes (
 	ID INT NOT NULL
-		CONSTRAINT PK_GroupAge PRIMARY KEY CLUSTERED,
+		CONSTRAINT PK_TeamsStatisticsTypes PRIMARY KEY CLUSTERED,
+	TeamsStatisticsTypes VARCHAR(255)
+)
+CREATE TABLE TeamsLeaguesStatistics (
+	ID INT NOT NULL 
+		CONSTRAINT PK_TeamsSeasonStatistics PRIMARY KEY CLUSTERED,
+	LeagueID INT NOT NULL,--DEPENDS ON th implementationn
+	TimePeriodId INT NULL
+		CONSTRAINT FK_TeamsLeaguesStatistics_TimePeriods FOREIGN KEY REFERENCES TimePeriods(ID),
+	TeamsStatisticsTypesId INT NULL
+		CONSTRAINT FK_TeamsLeaguesStatistics_TeamsStatisticsTypesId FOREIGN KEY REFERENCES TeamsStatisticsTypes(ID),
+	PossessionPercentage FLOAT NULL,
+	PassPercentage FLOAT NULL,
+	Wins SMALLINT NULL,
+	Losses SMALLINT NULL,
+	NumberOfMatches INT NULL,
+
+	YellowCards INT NULL,
+	RedCards INT NULL,
+	GoalsScored SMALLINT NULL,
+	GoalsScoredSixYardBox FLOAT NULL,
+	GoalsScoredOutOfBox FLOAT NULL,
+	GoalsScoredPenaltyArea FLOAT NULL,
+
+	GoalsCondenced SMALLINT NULL,
+	GoalsPerGame FLOAT NULL,
+	PassesPerGame FLOAT NULL,
+	RedCardsPerGame FLOAT NULL,
+	YellowCardsPerGame FLOAT NULL,
+	OffsidesPerGame FLOAT NULL,
+	ShotsOnGoalPerGame FLOAT NULL,
+	TacklesPerGame FLOAT NULL,
+	FouldPerGame FLOAT NULL,
+	DribblesPerGame FLOAT NULL,
+	FouledPerGame FLOAT NULL,
+
+
+
+)
+CREATE TABLE AgeCategories (
+	ID INT NOT NULL
+		CONSTRAINT PK_AgeCategories PRIMARY KEY CLUSTERED,
 	Name VARCHAR(255) NOT NULL
 )
 CREATE TABLE TeamTypes (
@@ -23,42 +62,17 @@ CREATE TABLE TeamTypes (
 CREATE TABLE Teams (
 	ID INT NOT NULL
 		CONSTRAINT PK_Teams PRIMARY KEY CLUSTERED,
-	GroupAgeId INT NOT NULL
-		CONSTRAINT FK_Teams_GroupsAge FOREIGN KEY REFERENCES GroupsAge(ID),
+	TeamGender BIT,
+	AgeCategoryId INT NOT NULL
+		CONSTRAINT FK_Teams_AgeCategories FOREIGN KEY REFERENCES AgeCategories(ID),
 	TeamTypeId INT NOT NULL
 		CONSTRAINT FK_Teams_TeamTypes FOREIGN KEY REFERENCES TeamTypes(ID),   
-	TeamGenderId INT NOT NULL
-		CONSTRAINT FK_Teams_Genders FOREIGN KEY REFERENCES Genders(ID),
 	Name VARCHAR(1000) NOT NULL,
-	CountryId INT NOT NULL, --FOREIGN
+	CountryId INT NOT NULL,
 	EstablishmentDate DATE NOT NULL	
 )
 
 
-CREATE TABLE TeamAttributes(
-	ID INT NOT NULL
-		CONSTRAINT PK_TeamAttributes PRIMARY KEY CLUSTERED,
-	TeamID INT NOT NULL
-		CONSTRAINT FK_TeamAttributes_Teams FOREIGN KEY REFERENCES Teams(ID),
-	ParametersDate DATE NULL,
-)
-
-CREATE TABLE Parametres (
-	ID INT NOT NULL
-		CONSTRAINT PK_Parametres PRIMARY KEY CLUSTERED,
-	Name VARCHAR(255) NOT NULL,
-	
-)
-
-CREATE TABLE ParametresTeams(
-	ID INT NOT NULL
-		CONSTRAINT PK_ParametresTeams PRIMARY KEY CLUSTERED,
-	TeamAttributesID INT NOT NULL	
-		CONSTRAINT FK_ParametresTeams_TeamAttributes FOREIGN KEY REFERENCES TeamAttributes(Id),
-	ParametreID INT NOT NULL
-		CONSTRAINT FK_ParametresTeams_Parametres FOREIGN KEY REFERENCES Parametres(Id),
-	Value INT NULL
-)
 
 
 CREATE TABLE Matches (
@@ -91,6 +105,14 @@ CREATE TABLE RankingTeams (
 	PreviousSeasonRankingId INT NULL
 		CONSTRAINT FK_RankingTeams_RankingTeams FOREIGN KEY REFERENCES RankingTeams(ID),
 	RankChange SMALLINT NULL,
+	ThreeYearsAgoAverage FLOAT NULL,
+	ThreeYearsAgoWeighted FLOAT NULL,
+	TwoYearsAgoAverage FLOAT NULL,
+	TwoYearsAgoWeighted FLOAT NULL,
+	LastYearAgoAverage FLOAT NULL,
+	LastYearAgoWeighted FLOAT NULL,
+	CurrentYearAgoAverage FLOAT NULL,
+	CurrentYearAgoWeighted FLOAT NULL
 )
 
 CREATE TABLE Standings ( 
