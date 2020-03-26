9eabc50ce72e4a77f1349a0e38f1658271bff0be
Dosya <alybaev_d@auca.kg>
2020-03-26 15:57:46 +0600

    Update

--- TeamsPart.sql
+++ TeamsPart.sql
@@ -1,42 +1,86 @@
 
 USE FootbalTeams
-DROP TABLE IF EXISTS Standings, TeamsMatches,TeamsLeagues, Teams,  RankingTeams,RankingSystems
+DROP TABLE IF EXISTS RankingTeams, Standings, TeamsMatches
+DROP TABLE IF EXISTS ParametresTeams, TeamAttributes,Parametres, TeamsLeagues,  Teams,   RankingSystems
+DROP TABLE IF EXISTS Genders, GroupsAge, TeamTypes
+
+CREATE TABLE Genders (
+	ID INT NOT NULL 
+		CONSTRAINT PK_Gender PRIMARY KEY CLUSTERED,
+	Gender BIT
+)
+CREATE TABLE GroupsAge (
+	ID INT NOT NULL
+		CONSTRAINT PK_GroupAge PRIMARY KEY CLUSTERED,
+	Name VARCHAR(255) NOT NULL
+)
+CREATE TABLE TeamTypes (
+	ID INT NOT NULL
+		CONSTRAINT PK_TeamType PRIMARY KEY CLUSTERED,
+	Name VARCHAR(255) NOT NULL
+)
 CREATE TABLE Teams (
 	ID INT NOT NULL
 		CONSTRAINT PK_Teams PRIMARY KEY CLUSTERED,
-	TeamAgeId INT NOT NULL, --десткая,молодежная
-	TeamType INT NOT NULL,   -- национальная, клуб
-	TeamGender INT NOT NULL, -- женская, мужская
+	GroupAgeId INT NOT NULL
+		CONSTRAINT FK_Teams_GroupsAge FOREIGN KEY REFERENCES GroupsAge(ID),
+	TeamTypeId INT NOT NULL
+		CONSTRAINT FK_Teams_TeamTypes FOREIGN KEY REFERENCES TeamTypes(ID),   
+	TeamGenderId INT NOT NULL
+		CONSTRAINT FK_Teams_Genders FOREIGN KEY REFERENCES Genders(ID),
 	Name VARCHAR(1000) NOT NULL,
 	CountryId INT NOT NULL, --FOREIGN
-	EstablishmentDate DATE NOT NULL
+	EstablishmentDate DATE NOT NULL	
 )
-CREATE TABLE TeamsMatches (
-	ID INT NOT NULL
-		CONSTRAINT PK_TeamsMatches PRIMARY KEY CLUSTERED,
-	TeamID INT NOT NULL
-		CONSTRAINT FK_Teams_Matches FOREIGN KEY REFERENCES Teams(ID),
-	MatchID INT NOT NULL, --FOREIGN
-	SomeStatistics INT NOT NULL
 
+
+CREATE TABLE TeamAttributes(
+	ID INT NOT NULL
+		CONSTRAINT PK_TeamAttributes PRIMARY KEY CLUSTERED,
+	TeamID INT NOT NULL
+		CONSTRAINT FK_TeamAttributes_Teams FOREIGN KEY REFERENCES Teams(ID),
+	ParametersDate DATE NULL,
 )
+
+CREATE TABLE Parametres (
+	ID INT NOT NULL
+		CONSTRAINT PK_Parametres PRIMARY KEY CLUSTERED,
+	Name VARCHAR(255) NOT NULL,
+	
+)
+
+CREATE TABLE ParametresTeams(
+	ID INT NOT NULL
+		CONSTRAINT PK_ParametresTeams PRIMARY KEY CLUSTERED,
+	TeamAttributesID INT NOT NULL	
+		CONSTRAINT FK_ParametresTeams_TeamAttributes FOREIGN KEY REFERENCES TeamAttributes(Id),
+	ParametreID INT NOT NULL
+		CONSTRAINT FK_ParametresTeams_Parametres FOREIGN KEY REFERENCES Parametres(Id),
+	Value INT NULL
+)
+
+
 CREATE TABLE TeamsLeagues (
 	ID INT NOT NULL
 		CONSTRAINT PK_TeamsLeagues PRIMARY KEY CLUSTERED,
 	TeamID INT NOT NULL
 		CONSTRAINT FK_Teams_Leagues FOREIGN KEY REFERENCES Teams(ID),
 	LeagueID INT NOT NULL, --FOREIGN
-	SomeStatistics INT NOT NULL
 
 )
+
 CREATE TABLE RankingSystems (
 	ID INT NOT NULL
 		CONSTRAINT PK_RankingSystem PRIMARY KEY CLUSTERED,
+	Name VARCHAR(255) NOT NULL
 )
+
 CREATE TABLE RankingTeams (
 	ID INT NOT NULL
 		CONSTRAINT PK_RankingTeams PRIMARY KEY CLUSTERED,
 	SeasonID INT NULL, --FOREIGN KEY
+	TeamID INT NOT NULL
+		CONSTRAINT FK_RankingTeams_Teams FOREIGN KEY REFERENCES Teams(ID),
 	TeamRank INT NOT NULL,
 	RankDate DATE NULL,
 	RatingSystemId INT NULL
